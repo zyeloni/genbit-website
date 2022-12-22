39b729ff04b99bf49173610e7267f7736fb5212f
Kacper Łukasik <k.lukasik@blunovation.com>
2022-12-22 02:27:15 +0100

    dodanie logowanie

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    /**
+     * @param UserPasswordHasherInterface $hasher
+     */
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail("genbit@uph.edu.pl");
 
+        $password = $this->hasher->hashPassword($user, "P@ssWord1!");
+        $user->setPassword($password);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
